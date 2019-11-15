ba8d4523b94b36c1b0a8bcd875d0546b998a0c33
Mick Stevens <mickstevens@gmail.com>
2019-11-15 15:02:55 +0000

    update packages
    updated packages & minor changes to App.js

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>***********</Text>
+      <Text>This is my First React Native App!</Text>
     </View>
   );
 }
@@ -12,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
